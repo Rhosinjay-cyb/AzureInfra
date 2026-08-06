99ac95dec406109591a0f5b1c79bb7cb6d087ce1
Rhosinjay-cyb <rosenjeabdullahi@gmail.com>
2026-08-06 19:55:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,9 +14,9 @@ provider "azurerm" {
   features {}
 }
 
-##############################
+###########################
 # Variables
-##############################
+###########################
 
 variable "admin_password" {
   description = "Windows VM Administrator Password"
