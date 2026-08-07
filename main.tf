560e9e45ae4885ed71d3aaceb3a4a8f7506a1c55
Rhosinjay-cyb <rosenjeabdullahi@gmail.com>
2026-08-07 09:59:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,13 @@ terraform {
     }
   }
 
+backend "azurerm" {
+    resource_group_name  = "rg-terraformstate"
+    storage_account_name = "sacctterraformstate"
+    container_name       = "contfstate"
+    key                  = "AzureInfra.contfstate"
+  }
+
   }
 
 
